18bf256ebec9e6d28527327dc73620bfa7841924
bahaa-abouzahr <bahaa.abouzahr@gmail.com>
2023-11-27 14:35:51 +0100

    Last Version

--- static/main.js
+++ static/main.js
@@ -5,3 +5,11 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navList.classList.toggle('active');
 });
+
+const typed = new Typed('.multiple', {
+    strings: ['Front-End Developer', 'IT Specialist'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:100,
+    loop:true
+});
